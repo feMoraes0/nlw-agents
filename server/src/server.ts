c0de903195d9ced0e37e9805ddded3c7b5290e3f
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-07-08 16:41:03 +0100

    server: adds and configures environment variables with zod validation

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ import {
   serializerCompiler,
 } from "fastify-type-provider-zod";
 import { fastifyCors } from "@fastify/cors";
+import { env } from "./env.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -15,6 +16,6 @@ app.register(fastifyCors, {
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log("Server is running!");
 });
