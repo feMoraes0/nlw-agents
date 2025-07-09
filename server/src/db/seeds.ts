b88a581b3f4c462ad1966209e30915cae646a6b3
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-07-09 21:24:43 +0100

    server: creates seeds

---
+++ server/src/db/seeds.ts
@@ -0,0 +1,20 @@
+import { reset, seed } from "drizzle-seed";
+import { db, sql } from "./connection.ts";
+import { schema } from "./schemas/index.ts";
+
+// clean database
+await reset(db, schema);
+// adds the new entries
+await seed(db, schema).refine((f) => {
+  return {
+    rooms: {
+      count: 20, // quantity of entries
+      columns: {
+        name: f.companyName(),
+        description: f.loremIpsum(),
+      },
+    },
+  };
+});
+// end the connection
+await sql.end();
